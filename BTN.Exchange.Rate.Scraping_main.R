174bc5ecf44a16e7a5780107c711f15618c2011f
Tandin Dorji <tndn.dorji@gmail.com>
2025-09-04 18:16:31 +0600

    Update BTN.Exchange.Rate.Scraping_main.R

--- BTN.Exchange.Rate.Scraping_main.R
+++ BTN.Exchange.Rate.Scraping_main.R
@@ -30,18 +30,21 @@ source("ScrapeRMAGoldSilver.R")
 goldsil_df <- ScrapeRMAGoldSilver()
 
 
-# save files
+# save files, 
+# add 21600 seconds or 6 hours for BST in file name 
+# as system time is in UTC
 forex_filename <- paste0("data/", 
-                         format(Sys.time(), "%Y%m%d-%H%M%S"), ".csv")
+                         format(Sys.time() + 21600, "%Y%m%d-%H%M%S"), ".csv")
 write.csv(forex_df, forex_filename, row.names = FALSE)
 
 
 goldsil_filename <- paste0("data/goldsilver/", 
-                           format(Sys.time(), "%Y%m%d-%H%M%S"), ".csv")
+                           format(Sys.time() + 21600, "%Y%m%d-%H%M%S"), ".csv")
 write.csv(goldsil_df, goldsil_filename, row.names = FALSE)
 
 
 rm(list=ls())
 gc()
 cat("\014")
+
 # q()
